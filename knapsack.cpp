44372f0ab338aebb6b4227a620eaa4c5771afbf2
Ibrahimmushtaq98 <ibrahim.mushtaq@uoit.net>
2019-04-08 01:34:03 -0400

    Added a csv parser that returns array, Made a knapsack func with array

--- knapsack.cpp
+++ knapsack.cpp
@@ -18,8 +18,9 @@ struct Item{
   int weight;
 };
 
-//Parses through a .CSV file and return a vector of type Item
-std::vector<Item> readCSV();
+//Parses through a .CSV file and return either vector or array of type Item
+std::vector<Item> readVecCSV();
+Item* readArrCSV();
 
 //Finds the sum of all the values of the item
 int sum(std::vector<struct Item> items);
@@ -28,9 +29,10 @@ int max(int a, int b);
 Prints out the optimal solution of the knapsack problem with the given weights
 */
 void knapsack(std::vector<struct Item> items, int carryWeight);
-/*
-Recursively computes the knapsack problem on subsets of the actual set
-*/
+void knapsackArr(Item *item, int carryWeight);
+
+int globalSize;
+
 int main(int argc, char const *argv[]) {
 
   //Dummy data
@@ -49,10 +51,12 @@ int main(int argc, char const *argv[]) {
 
   int carryWeight = 5;
 
-    std::vector<struct Item> dummy = readCSV();
+    std::vector<struct Item> dummy = readVecCSV();
+    Item *itemsman = readArrCSV();
 
   knapsack(dummy, carryWeight);
-  
+  knapsackArr(itemsman, carryWeight);
+  free(itemsman);
   return 0;
 }
 
@@ -89,6 +93,38 @@ void knapsack(std::vector<struct Item> items, int carryWeight){
   std::cout << knapsackMat[items.size()-1][carryWeight] << std::endl;
 }
 
+//knapsack function, but implimented in arrays
+void knapsackArr(Item* items, int carryWeight){
+  int knapsackMat[globalSize][carryWeight+1];
+   //A backpack that has a carryWeight of 0 can't carry anything
+  for(int y = 0; y < globalSize; y ++){
+    knapsackMat[y][0] = 0;
+  }
+  
+  for(int x = 1; x < carryWeight + 1; x ++){
+    for(int y = 0; y < globalSize; y ++){
+      struct Item currentItem = items[y];
+      int actualVal;
+      if(y == 0){
+        if(currentItem.weight <= carryWeight){
+          actualVal = currentItem.value;
+        }
+      }else{
+        if(currentItem.weight <= x){ //The current item can fit in the bag
+          // What is better value? The all the previous items without the current item
+          // or the current item plus the items that would be allowed with the current item inside the bag?
+          actualVal = max(knapsackMat[y-1][x-currentItem.weight] + currentItem.value, knapsackMat[y-1][x]);
+  
+        }else{
+          actualVal = knapsackMat[y-1][x];
+        }
+      }
+      //std::printf("%d\n", actualVal);
+      knapsackMat[y][x] = actualVal;
+    }
+  }
+  std::cout << knapsackMat[globalSize-1][carryWeight] << std::endl;
+}
 
 int sum(std::vector<struct Item> items){
   int sum = 0;
@@ -107,7 +143,7 @@ int max(int a, int b){
 
 //A function that reads through a csv file
 //The structure of that CSV file is value,weight
-std::vector<Item> readCSV(){
+std::vector<Item> readVecCSV(){
     std::vector<struct Item> items;
     std::string line;
     std::string value;
@@ -123,6 +159,8 @@ std::vector<Item> readCSV(){
         exit(1);
     }
 
+    std::getline(file, line);
+
     //Read through the file line by line
     while(std::getline(file, line)){
         //Creates a string stream such that it fits the getline param
@@ -136,5 +174,55 @@ std::vector<Item> readCSV(){
         items[items.size() - 1].weight = std::stoi(weight);
     }
 
+    return items;
+}
+
+//A function that reads through a csv file and returns a
+//array
+Item* readArrCSV(){
+    std::string line;
+    std::string value;
+    std::string weight;
+    std::ifstream file;
+
+    //Opens the file
+    file.open("knapsack.csv");
+
+    //Check if the file exist or if there are any error
+    if(!file){
+        std::cerr << "Unable to open file!\n";
+        exit(1);
+    }
+
+    //Since the first line of the file contains the size information
+    //This will get the size info
+    std::getline(file, line);
+    std::cout << line << std::endl;
+
+    //Since this function returns an pointer, which is imediatly going to get 
+    //dealocated, this would mean //Item items[std::stoi(line)]; would just point
+    //to an empty location. To counter this, we must allocate memory space and delete
+    //afterward
+    Item* items = (Item*) malloc(std::stoi(line) * sizeof(Item));
+
+    //Unfortunatly, malloc returns a pointer which by default, returns a type void
+    //We must track the size our self, hence global, as the global variable
+    globalSize = std::stoi(line);
+    int count = 0;
+
+    //Read through the file line by line
+    while(std::getline(file, line)){
+        //Creates a string stream such that it fits the getline param
+        std::stringstream ss(line);
+
+        std::getline(ss,value, ',');
+        std::getline(ss,weight, '\n');
+
+        items[count].value = std::stoi(value);
+        items[count].weight = std::stoi(weight);
+
+        //std::printf("Value: %d Weight: %d i: %d\n",items[count].value, items[count].weight, count);
+        count++;
+    }
     return items;
 }
