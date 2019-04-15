163e18bd59be6bec39cd82c023747313154997be
Colin Orian <corian@rogers.com>
2019-04-14 21:00:27 -0400

    Removed extra files and code.

--- seqScript.sh
+++ seqScript.sh
@@ -1,6 +1,6 @@
 touch itemsFixed.dat
 echo "number of items fixed:"
-python3 makeData.py 100
+python3 makeData.py 1000
 for i in `seq 9`
 do
     echo "Capacity = 10^$i" >> itemsFixed.dat
@@ -14,13 +14,4 @@ do
     echo "Items = 10^$i" >> capcityFixed.dat
     python3 makeData.py $((10**i))
     ./a.out 1000 "madeData.csv" >> capcityFixed.dat
-done
-
-touch neitherFixed.dat
-echo "Neither fixed"
-for i in `seq 9`
-do
-    echo "Items and capacity = 10^$i" >> neitherFixed.dat
-    python3 makeData.py $((10**i))
-    ./a.out $((10**i)) "madeData.csv" >> neitherFixed.dat
 done
