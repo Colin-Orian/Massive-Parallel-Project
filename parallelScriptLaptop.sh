ab60d4f6d25289818a12de4bb13ce2200ab3b533
Colin Orian <corian@rogers.com>
2019-04-14 21:46:26 -0400

    forgot server doesn't have python3. changed to python3.6

--- parallelScriptLaptop.sh
+++ parallelScriptLaptop.sh
@@ -2,7 +2,7 @@ touch itemsFixedPar.dat
 rm itemsFixedPar.dat
 touch itemsFixedPar.dat
 echo "number of items fixed:"
-python3 makeData.py 1000
+python3.6 makeData.py 1000
 for i in `seq 9`
 do
     echo "Capacity = 10^$i" >> itemsFixedPar.dat
@@ -16,6 +16,6 @@ echo "Bag Capacity fixed"
 for i in `seq 9`
 do
     echo "Items = 10^$i" >> capacityFixedPar.dat
-    python3 makeData.py $((10**i))
+    python3.6 makeData.py $((10**i))
     ./parallel.out 1000 "madeData.csv" 8 >> capacityFixedPar.dat
 done
--- seqScript.sh
+++ seqScript.sh
@@ -2,7 +2,7 @@ touch itemsFixedSeq.dat
 rm itemsFixedSeq.dat
 touch itemsFixedSeq.dat
 echo "number of items fixed:"
-python3 makeData.py 1000
+python3.6 makeData.py 1000
 for i in `seq 9`
 do
     echo "Capacity = 10^$i" >> itemsFixedSeq.dat
@@ -16,6 +16,6 @@ echo "Bag Capacity fixed"
 for i in `seq 9`
 do
     echo "Items = 10^$i" >> capacityFixedSeq.dat
-    python3 makeData.py $((10**i))
+    python3.6 makeData.py $((10**i))
     ./sequental.out 1000 "madeData.csv" >> capacityFixedSeq.dat
 done
